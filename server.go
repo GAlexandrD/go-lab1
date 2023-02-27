8bcee4d5894ff4a8bdf8616e2f1c9c3318185272
vladmoroca <vladmoroca@gmail.com>
2023-02-27 19:29:12 +0200

    add getTime function

--- server.go
+++ server.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 type TimeData struct {
     Time string json:"time"
 }
+
+func getTime(w http.ResponseWriter, _ *http.Request) {
+	t := TimeData{Time: time.Now().Format(time.RFC3339)}
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(t)
+  }
 
 func main() {
     fmt.Println("Hello, world.")
